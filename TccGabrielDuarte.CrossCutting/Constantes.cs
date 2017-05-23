8510e855bbc9bb4f2826261f7fe83e26d05e565a
Gabriel Duarte <gabrielqduarte@gmail.com>
2017-05-22 22:35:02 -0300

    Passando o sql server para a nuvem

--- TccGabrielDuarte.CrossCutting/Constantes.cs
+++ TccGabrielDuarte.CrossCutting/Constantes.cs
@@ -7,7 +7,7 @@ namespace TccGabrielDuarte.CrossCutting
 {
     public class Constantes
     {
-        public const string CONN_SQLSERVER = @"Server=(localdb)\mssqllocaldb;Database=SqlServer;Trusted_Connection=True;";
+        public const string CONN_SQLSERVER = @"Server=tcp:tccgabrielduarte.database.windows.net,1433;Initial Catalog=TccGabrielDuarte;Persist Security Info=False;User ID=gabrielduartem;Password=123pass321$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public const string CONN_SQLITE = "Data Source=Sqlite.db";
     }
 }
